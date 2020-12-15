a1e3b7ce3a7db95230044cde50c3093b9af69fa5
Abhinav Robinson <abhinavrobinson@gmail.com>
2020-12-15 12:38:49 +0530

    (refactor): prettify ⭐

---
+++ js/grids.js
@@ -0,0 +1,27 @@
+// Active isotope with jQuery code:
+
+
+var $grid = $('.grid').imagesLoaded( function() {
+    // init Isotope after all images have loaded
+    $grid.isotope({
+      itemSelector: '.grid-item',
+        percentPosition: true,
+      gutter: 0,
+      masonry: {
+          // use outer width of grid-sizer for columnWidth
+          columnWidth: '.grid-sizer'
+      }
+    });
+  });
+  
+  // Isotope click function
+  $('.iso-nav a').click(function(){
+      $('.iso-nav a').removeClass('active');
+      $(this).addClass('active');
+  
+      var selector = $(this).attr('data-filter');
+      $('.grid').isotope({
+          filter: selector
+      });
+      return false;
+  });
